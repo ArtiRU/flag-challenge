b96de4f462aacf1a95ec6ddc5ab8ec131bafb84e
ArtiRU <gignt0646@mail.ru>
2022-03-21 15:10:44 +0300

    Add component container

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    max-width: 1440px;
+    margin: 0 auto;
+    padding: 0 2rem;
+`;
+
+export default Container;
