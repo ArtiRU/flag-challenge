040ec0751e55d39ef19adf8d959c396efbcfe29c
ArtiRU <gignt0646@mail.ru>
2022-03-23 17:14:18 +0300

    Update homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Controls from "../../components/Controls/Controls";
+import api from '../../api/config';
+import useCountries from "../../hooks/useCountries";
+import List from "../../components/List/List";
+import Card from "../../components/Card/Card";
+import {ALL_COUNTRY} from "../../utils/consts";
 
 const HomePage = () => {
+    const {countries, setCountries} = useCountries();
+
+    useEffect(() => {
+        api.get(ALL_COUNTRY).then(({data}) => setCountries(data));
+    }, []);
+
     return (
         <>
             <Controls />
+            <List>
+                {
+                    countries.map(c => <Card key={c.name} {...c} />)
+                }
+            </List>
         </>
     );
 };
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://restcountries.com/v2/'
+export const BASE_URL = 'https://restcountries.com/v2/';
+export const ALL_COUNTRY = '/all?fields=name,flag,capital,region,population';
