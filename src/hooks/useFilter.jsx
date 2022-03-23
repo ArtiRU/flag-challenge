8f1f3ca7af380fa8b9489ecbdf97fef8e76899ff
ArtiRU <gignt0646@mail.ru>
2022-03-23 18:14:00 +0300

    Add hook useFilter

---
+++ src/hooks/useFilter.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import useCountries from "./useCountries";
+
+const useFilter = () => {
+    const {countries} = useCountries();
+    const [filtered, setFiltered] = useState([]);
+
+    const onFilter = (search, region) => {
+        let data = [...countries];
+        if (search) {
+            data = data.filter(d => d.name.toLowerCase().includes(search.toLowerCase()));
+        }
+        if (region) {
+            data = data.filter(d => d.region.includes(region));
+        }
+        setFiltered(data);
+    };
+
+
+    return {
+        filtered,
+        onFilter
+    };
+};
+
+export default useFilter;
