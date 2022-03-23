9dbdc21ee58d544f9e2825e6f23bfa4419879ab0
ArtiRU <gignt0646@mail.ru>
2022-03-23 17:11:20 +0300

    Add component list

---
+++ src/components/List/list-styling.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 2rem;
+    
+    margin-top: 2rem;
+`;
