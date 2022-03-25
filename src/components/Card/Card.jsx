667160e02bd1662b098d30886421be0ec800afb0
ArtiRU <gignt0646@mail.ru>
2022-03-25 09:10:05 +0300

    Add card click handler

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import {CardBody, CardImage, CardInfo, Title, Wrapper} from "./card-styling";
+import {searchByCountry} from '../../api/endpoints';
 
 
 const Card = ({flag, population, name, region, capital}) => {
+    const navigate = useNavigate();
     const localPopulation = population.toLocaleString().replace(/\s/g, ',');
 
+    const onCardClick = () => navigate(searchByCountry(name));
+
     return (
-        <Wrapper>
+        <Wrapper onClick={onCardClick}>
             <CardImage src={flag} alt={name} />
             <CardBody>
                 <Title>{name}</Title>
