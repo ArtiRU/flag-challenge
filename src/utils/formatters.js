f5426c3ef9af2b5df0f50c1fe44d50f16c59c0d4
ArtiRU <gignt0646@mail.ru>
2022-03-26 15:13:18 +0300

    Fix type error function and add new func formatNumber

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,2 +1,3 @@
+export const formatPopulation = (number) => number?.toLocaleString().replace(/\s/g, ',');
 
-export const formatPopulation = (number) => number.toLocaleString().replace(/\s/g, ',');
+export const formatNumber = (number, index, size) => size - 1 === index ? number : number + ',';
